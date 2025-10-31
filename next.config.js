4d1ad5080cd906e651739a992a2462d08fce6ef0
ZarakiKinp <businessmk17@gmail.com>
2025-10-31 11:49:02 +0100

    Exclude client folder from Next.js build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     KOMMO_API_KEY: process.env.NEXT_PUBLIC_KOMMO_ACCESS_TOKEN,
     NEXT_PUBLIC_OPENAI_API_KEY: process.env.OPENAI_API_KEY || process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   },
+  // Exclude client folder from build
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/client/**', '**/node_modules/**'],
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
